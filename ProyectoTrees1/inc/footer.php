188b88bd362c3e7d960f8d14f77ffeca2aa5df19
angiehh10 <angiehhlobo10@gmail.com>
2024-11-04 21:42:46 -0600

    Delete ProyectoTrees1 directory

--- ProyectoTrees1/inc/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="bg-success text-white text-center py-3 mt-auto w-100">
-    <p class="mb-0">&copy; 2024 MyTrees - Todos los derechos reservados.</p>
-</footer>
-
-<!-- Bootstrap JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
